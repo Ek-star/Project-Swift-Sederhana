cb0611ed7dd2f0716f7671d0b53f77afd5a5e9f6
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2024-08-08 10:27:21 +0700

    Initial Commit

---
+++ DicodingApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicodingApps
+//
+//  Created by EKALMA TOTO ALLOY SEMBIRING on 08/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
