9762e5de9ddd04138debbd11d3b4d6c7944c76aa
Ek-star <totoekalma@gmail.com>
2024-08-08 14:52:01 +0700

    Swift

---
+++ DicodingApps/AcademyModel.swift
@@ -0,0 +1,8 @@
+//
+//  AcademyModel.swift
+//  DicodingApps
+//
+//  Created by EKALMA TOTO ALLOY SEMBIRING on 08/08/24.
+//
+
+import Foundation
---
+++ DicodingApps/AcademyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AcademyTableViewCell.swift
+//  DicodingApps
+//
+//  Created by EKALMA TOTO ALLOY SEMBIRING on 08/08/24.
+//
+
+import UIKit
+
+class AcademyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
